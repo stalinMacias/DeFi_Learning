96dd305ee02fb38e3b024c9d4a894e00205f9c64
Stalin Macias <mags961224hjccmt00@gmail.com>
2022-09-04 19:48:51 -0500

    Fixed the deployment of contracts in Ganache - UniswapV2Artifactory.json was missing the Contract Name

--- migrations/2_Uniswapv2Factory.js
+++ migrations/2_Uniswapv2Factory.js
@@ -3,9 +3,7 @@ const Uniswapv2Factory = artifacts.require("UniswapV2Factory");
 
 //const feeAddress = "0x3782897C2aA7291b148d2C02BB54F7bC84982360";
 
-
-
-module.exports = function (deployer) {
+module.exports = async function (deployer) {
   //console.log(Uniswapv2Factory.bytecode)
-  deployer.deploy(Uniswapv2Factory, "0x3782897C2aA7291b148d2C02BB54F7bC84982360")
+  await deployer.deploy(Uniswapv2Factory, "0x3782897C2aA7291b148d2C02BB54F7bC84982360")
 }
--- migrations/3_LiquidityValueCalculator.js
+++ migrations/3_LiquidityValueCalculator.js
@@ -3,6 +3,6 @@ const LiquidityValueCalculator = artifacts.require("LiquidityValueCalculator");
 
 //const FactoryContractAddress = "0x496F09eD079883D7b15ec588c2D8567F1193FBc0"
 
-module.exports = function (deployer) {
-  deployer.deploy(LiquidityValueCalculator, "0x208341E054c9eB3b3e42BF45857fB0593E05847a")
+module.exports = async function (deployer) {
+  await deployer.deploy(LiquidityValueCalculator, "0x208341E054c9eB3b3e42BF45857fB0593E05847a")
 }
